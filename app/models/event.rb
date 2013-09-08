b1647f33e11c7740e7902ed2dff8b6f28b7a229b
Alex Fiore <writefiore@gmail.com>
2013-09-08 15:04:35 -0700

    added validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,16 @@
 class Event < ActiveRecord::Base
+
+  validates :title, presence: true, uniqueness: true
+  validates :organizer_name, presence: true
+  validates :organizer_email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i }
+
+  validates :date, presence: true, message: "Date field cannot be empty."
+  validates :date_is_valid
+
+  def date_is_valid
+    if date.present? && date < (Date.today)
+      errors.add(:date, 'Date cannot be in the past.')
+    end
+  end
+
 end
--- db/migrate/20130527111009_create_events.rb
+++ db/migrate/20130527111009_create_events.rb
@@ -1,4 +1,4 @@
-class CreateEvents < ActiveRecord::Migration
+  class CreateEvents < ActiveRecord::Migration
   def change
     create_table :events do |t|
       t.string :organizer_name, :organizer_email, :title
